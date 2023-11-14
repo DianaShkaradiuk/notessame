b72bd959ec175099d1c64dac3830dea6899a8899
DianaShkaradiuk <dshkaradiuk23@fed.idserve.net>
2023-11-14 15:45:27 +0100

    Initial Commit

---
+++ notessame/notessameApp.swift
@@ -0,0 +1,17 @@
+//
+//  notessameApp.swift
+//  notessame
+//
+//  Created by Diana Shkaradiuk on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct notessameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
